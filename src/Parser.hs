35917bbc09286b679183ed3952e247a14ce3a1b4
Bertram Arnold <bertram.arnold@gmail.com>
2017-03-31 23:52:01 -0700

    Rehauled Parser function to properly parse system events and multiline messages.

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,16 +1,29 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell
+            ,TupleSections
+            ,OverloadedStrings
+            #-}
 
 module Parser where
 
 import Data.Dates
 import qualified Data.Text.Lazy as T
+import qualified Data.Text.Lazy.IO as I
 import Text.Megaparsec
 import Text.Megaparsec.Text.Lazy
 import Control.Lens
+import Control.Applicative (liftA2,liftA3)
 
 
-data User = User T.Text | Number T.Text deriving (Show)
-data Message = Media | Message T.Text deriving (Show)
+data User = User T.Text | Number T.Text deriving (Show,Eq)
+data Message = Join User -- added by User
+             | Quit -- Left
+             | Media -- Sent Media
+             | IconChange -- Changed Icon
+             | NumberChange -- Changed Number
+             | Create -- Created group
+             | Change -- Changed group subject
+             | Message T.Text -- Sent Message
+             deriving (Show)
 data Chunk = Chunk
              { _time :: DateTime
              , _user :: User
@@ -18,13 +31,12 @@ data Chunk = Chunk
              } deriving (Show)
 makeLenses ''Chunk
 
-userP :: Parser User
-userP = numberP <|> nameP
+userP :: Parser a -> Parser User
+userP end = Number . T.pack <$> liftA2 (:) (char '\8234' *> char '+')
+            (manyTill (digitChar <|> spaceChar) (char '\8236' *> end))
+            <|> User . T.pack <$> manyTill noNewlineChar end
   where
-    numberP = Number . T.pack <$> ((:) <$> char '+'
-              <*> manyTill (digitChar <|> spaceChar) (char ':'))
-    nameP = User . T.pack <$> manyTill anyChar (char ':')
-
+    noNewlineChar = notFollowedBy eol *> anyChar
 dateP :: Parser DateTime
 dateP = datify 
         <$> intP 2 -- day
@@ -41,27 +53,63 @@ dateP = datify
     intP n = read <$> count n digitChar
     datify d m y h mm = DateTime y m d h mm 0
 
-messageP :: Parser Message
-messageP = string "<Media omitted>" *> eol *> return Media
-         <|> Message <$> (T.append <$> firstLineP <*> otherLineP)
+firstLineP :: Parser Chunk
+firstLineP = liftA2 (\d (u,m) -> Chunk d u m) (dateP <* string " - ")
+                    userMessageP
   where
-    firstLineP :: Parser T.Text
-    firstLineP = T.pack <$> manyTill anyChar eol
-    otherLineP :: Parser T.Text
-    otherLineP = lookAhead (try (dateP <* string " - " <* userP *> return ())
-                            <|> eof)
-                 *> return (T.pack "")
-                 -- if we find a valid new line, end message
-                 <|> T.append <$> (T.pack . ('\n':) <$> manyTill anyChar eol)
-                     <*> otherLineP
-                 -- else parse line, append with newline, retry
+    userMessageP :: Parser (User,Message)
+    userMessageP =   try createP
+                 <|> try joinP
+                 <|> try quitP
+                 <|> try changeP
+                 <|> try numberChangeP
+                 <|> try iconChangeP
+                 <|> try mediaP
+                 <|> firstLineP'
+    createP = (,Create) <$> userP (string " created group ")
+                    <* manyTill anyChar eolf
+    joinP = liftA2 join' (userP (string " added ")) (userP eolf)
+    join' u1 u2 = (u2, Join u1)
+    quitP = (,Quit) <$> userP (string " left" <* eolf)
+    changeP = (,Change) <$> userP (string " changed the subject to ")
+                    <* manyTill anyChar eolf
+    iconChangeP = (,IconChange) <$> userP
+      (string " changed this group's icon" <* eolf)
+    numberChangeP = (,NumberChange) <$>
+      ((try (userP (string " changed from"))
+      <|> userP (string " changed to "))
+      <* manyTill anyChar eolf)
+    mediaP = (,Media) <$> userP (string ": <Media omitted>" <* eolf)
+    firstLineP' = liftA2 mess' (userP (string ": ")) (manyTill anyChar eolf)
+    mess' u t = (u, Message $ T.pack t)
+    eolf = eof <|> (eol *> return ())
 
-chunkP = Chunk
-         <$> dateP
-         <* string " - "
-         <*> userP
-         <* space
-         <*> messageP
+chunksP :: Parser [Chunk]
+chunksP =   eof *> return [] <|> (firstLineP >>= chunksP')
+  where
+    chunksP' :: Chunk -> Parser [Chunk]
+    chunksP' c@(Chunk _ _ m) = eof *> return [c] <|>
+      case m of  
+        Message t -> remNextChunk >>= updateAndParse
+          where
+            updateAndParse (ls,mc) = case mc of
+              Nothing  -> return [c']
+              Just c'' -> (c':) <$> chunksP' c''
+              where
+                c' = c & message .~ (Message . T.intercalate "\n" $ t:ls)
+        _ -> (c:) <$> chunksP
+remNextChunk =   eof *> return ([],Nothing)
+             <|> ([],) . Just <$> try firstLineP
+             <|> liftA2 (\l (ls,c) -> (l:ls,c)) line remNextChunk
+line = T.pack <$> manyTill anyChar (eof <|> eol *> return ())
+
+isMedia :: Message -> Bool
+isMedia Media = True
+isMedia _     = False
+
+isUser :: User -> Bool
+isUser (User _)   = True
+isUser (Number _) = False
       
 
 test :: (Show a) => Parser a -> String -> IO ()
@@ -69,3 +117,10 @@ test p = parseTest p . T.pack
 
 file :: String
 file = "/home/bertram/Downloads/Bonnsai.txt"
+
+bertram :: User
+bertram = User $ T.pack "Bertram"
+
+messages :: IO [Chunk]
+messages = (parseMaybe chunksP <$> I.readFile file)
+           >>= maybe (putStrLn "Parse failed" >> return []) return
